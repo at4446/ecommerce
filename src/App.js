181ed07fe1524b8c8e293eb3d8593ef260779bbd
Amir Torkashvand <atorkashvand@blackberry.com>
2020-10-05 20:38:01 -0400

    added rounting

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
+
 function App() {
   return (
-    <div >
-<HomePage></HomePage>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
+
 
 export default App;
